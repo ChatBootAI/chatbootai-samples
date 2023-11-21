ea76fc7ea32449bc52c0ac1f17a1639f2a5d8e79
Antonio Goncalves <antonio.goncalves@gmail.com>
2023-11-21 17:59:32 +0100

    feat(java): init Quarkus and Langchain sample

---
+++ java/sample-java-quarkus-langchain/src/test/java/ai/chatboot/sample/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package ai.chatboot.sample;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
